e2c6d211e4f58016858f72e5e7604376be8ba3fa
Rashmi Yenure <rashmi13yenure@gmail.com>
2021-08-23 17:38:55 +0530

    added popups in profile page

--- Fantum Customer Dashboard/assets/js/custom.js
+++ Fantum Customer Dashboard/assets/js/custom.js
@@ -146,3 +146,13 @@ if (sessionLang){
   //$('.btn-select').attr('value', 'en');
 }
 
+//====== Profile page============
+// Getting an instance of the widget.
+const widget = uploadcare.Widget('[role=uploadcare-uploader]');
+// Selecting an image to be replaced with the uploaded one.
+const preview = document.getElementById('preview');
+// "onUploadComplete" lets you get file info once it has been uploaded.
+// "cdnUrl" holds a URL of the uploaded file: to replace a preview with.
+widget.onUploadComplete(fileInfo => {
+  preview.src = fileInfo.cdnUrl;
+});
